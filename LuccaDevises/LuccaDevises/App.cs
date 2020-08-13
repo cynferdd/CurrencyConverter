b995c7e0e2492994f3d3bce100c4964838eb228a
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-08-13 09:41:47 +0200

    Setting up Dependency injection

---
+++ LuccaDevises/LuccaDevises/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuccaDevises
+{
+    public class App
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
--- LuccaDevises/LuccaDevises/Program.cs
+++ LuccaDevises/LuccaDevises/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace LuccaDevises
 {
@@ -6,7 +9,34 @@ namespace LuccaDevises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            // required to run the application
+            services.AddTransient<App>();
+
+            return services;
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
         }
     }
 }
