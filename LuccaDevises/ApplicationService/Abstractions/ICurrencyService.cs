632e4c0e8eaa582c9f882ab18bbc1866e41ff839
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-08-14 09:55:08 +0200

    adding CurrencyService

---
+++ LuccaDevises/ApplicationService/Abstractions/ICurrencyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationService.Abstractions
+{
+    public interface ICurrencyService
+    {
+        int CalculateRate(string filePath);
+    }
+}
---
+++ LuccaDevises/ApplicationService/CurrencyService.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationService.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationService
+{
+    public class CurrencyService : ICurrencyService
+    {
+        public CurrencyService()
+        {
+
+        }
+        public int CalculateRate(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
