9b707fc6b2b747c9e185bfd7214e35d82e6d14e5
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-08-14 16:34:30 +0200

    updating comments in path calculator to better reflect how the BFS algorithm works

--- LuccaDevises/DomainService/PathCalculator.cs
+++ LuccaDevises/DomainService/PathCalculator.cs
@@ -8,31 +8,39 @@ namespace DomainService
 {
     public class PathCalculator : IPathCalculator
     {
-        
-        
         public PathCalculator()
         {
-                        
-        }
-        
 
+        }
+
+        /// <summary>
+        /// Retrieving the shortest path from a source currency to a target currency, given a set of adjacent edges in a graph
+        /// </summary>
+        /// <param name="currencyPathes">list of change rates, possible pathes of currency graph</param>
+        /// <param name="source">source currency</param>
+        /// <param name="target">target currency</param>
+        /// <returns>shortest path from source currency to target currency</returns>
         public IList<Change> Rates(IList<Change> currencyPathes, string source, string target)
         {
             List<Change> pathes = new List<Change>();
             pathes.AddRange(currencyPathes);
 
             // adding inversed change rates to get all pathes and better performances
+            // since it is inversed, the rate must be modified to 1/rate, rounded to 4 decimals
             foreach (var item in currencyPathes)
             {
                 pathes.Add(new Change(item.TargetCurrency, item.SourceCurrency, Math.Round(1 / item.Rate, 4)));
             }
 
             List<List<Change>> foundPathes = FindRatesPathes(source, target, pathes.ToArray());
+            
+            // retrieving first path with minimum amount of steps
             return foundPathes.OrderBy(m => m.Count).FirstOrDefault();
         }
 
         /// <summary>
         /// Recursive search of possible pathes from baseCurrency to targetCurrency
+        /// Adapted from Breadth-First Search algorithm
         /// </summary>
         /// <param name="baseCurrency">currency from where the search starts</param>
         /// <param name="targetCurrency">Seeked currency</param>
